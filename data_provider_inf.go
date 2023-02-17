f5b3436039c666b849a7eb77fe29bedeb8b5337f
Sergey Leo Kallaur <sl@kallaur.ru>
2023-02-17 19:50:03 +0300

    Добавил тип пред подготовки данных для дата провайдеров.

--- data_provider_inf.go
+++ data_provider_inf.go
@@ -3,3 +3,6 @@ package termui
 type UpdatingDataProvider interface {
 	UpdateData(data string, address ...uint32)
 }
+
+// FDataProviderConverter - тип функций сборки и конвертации данных для дата провайдеров
+type FDataProviderConverter func() map[uint32]string
