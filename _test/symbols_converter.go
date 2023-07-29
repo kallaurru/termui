ddf32ab1e156d048e8278f6e008553353e017ac1
Sergey Leo Kallaur <sl@kallaur.ru>
2023-07-29 17:12:33 +0300

    добавил новый вариант форматирования сумм

--- _test/symbols_converter.go
+++ _test/symbols_converter.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/kallaurru/termui/v3"
+	"log"
 )
 
 func main() {
 	var input int32 = 1209039
+	var amount, dec int64 = -130000, 78
+	fmtStr := termui.FormatAmountToMonoNumbers(amount, int32(dec), true)
+	log.Println(fmtStr)
 	fmt.Println(termui.ConvertToMonoNumbers(input))
-	fmt.Println(termui.ConvertToFullNumbers(input))
-	fmt.Println(termui.ConvertSymToMarkers("BAL"))
+	log.Println(termui.ConvertToFullNumbers(input))
+	log.Println(termui.ConvertSymToMarkers("BAL"))
 }
--- utils_formatter.go
+++ utils_formatter.go
@@ -8,11 +8,11 @@ import (
 // @decimal - если меньше 0, значит вывод десятичных отключаем
 func FormatAmount(amount, decimal int32, isMono bool, addCurSym bool) string {
 	var amountStr, decimalStr string
-	useDecimal := decimal >= 0
-
+	useDecimal := decimal != 0
 	if decimal < 0 {
-		decimal = 0
+		decimal = -1 * decimal
 	}
+
 	if useDecimal {
 		if decimal < 10 {
 			if isMono {
@@ -33,6 +33,44 @@ func FormatAmount(amount, decimal int32, isMono bool, addCurSym bool) string {
 		amountStr = ConvertToMonoNumbers(amount)
 	} else {
 		amountStr = ConvertToFullNumbers(amount)
+	}
+
+	if addCurSym {
+		return fmt.Sprintf("%s%s,%s", string(RUR), amountStr, decimalStr)
+	}
+	if useDecimal {
+		return fmt.Sprintf("%s,%s", amountStr, decimalStr)
+	} else {
+		return amountStr
+	}
+}
+
+func FormatAmountToMonoNumbers(amount int64, decimal int32, addCurSym bool) string {
+	var amountStr, decimalStr string
+
+	useDecimal := decimal != 0
+	isNegative := amount < 0
+
+	if decimal < 0 {
+		decimal = -1 * decimal
+	}
+
+	minusStr := string(SYMBOL_MATH_MINUS)
+
+	if useDecimal {
+		if decimal < 10 {
+			decimalStr = fmt.Sprintf("%s%s", ConvertToMonoNumbers(0), ConvertToMonoNumbers(decimal))
+		} else {
+			decimalStr = fmt.Sprintf("%s", ConvertToMonoNumbers(decimal))
+		}
+	} else {
+		decimalStr = ""
+	}
+
+	if isNegative {
+		amountStr = fmt.Sprintf(" %s%s", minusStr, ConvertToMonoNumbers(int32(amount)))
+	} else {
+		amountStr = ConvertToMonoNumbers(int32(amount))
 	}
 
 	if addCurSym {
